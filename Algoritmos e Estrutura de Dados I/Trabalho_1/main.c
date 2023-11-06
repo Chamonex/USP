032e5a2506bbc9ca5d938f4c2d6e1d74af2b5f51
Pedro Chamone <chamonepe@gmail.com>
2023-11-06 16:27:26 -0300

    AED: trabalho skiplist: tentativa de entrega 2, strtok

--- Algoritmos e Estrutura de Dados I/Trabalho_1/main.c
+++ Algoritmos e Estrutura de Dados I/Trabalho_1/main.c
@@ -7,12 +7,48 @@ void main() {
     SkipList *l = criaSkipList(10, 0.5);
     char opt = 'a';
     char nome[20];
-    int tel, len;
+    char entrada[42];
+    char *atual;
+    char tel[20];
+    int cont = 0;
+    int len;
+    
     
     while(opt != '0') {
-        opt = getchar();
-        getchar();
 
+        // REPARTIR ENTRADA
+        atual = strtok(entrada, " ");
+
+        while (atual != NULL) {
+
+            if (cont == 0) {
+                printf("OPCAO\n");
+                opt = atual[0];
+                
+            }
+
+            if (cont == 1) {
+                printf("NOME\n");
+                len = strlen(atual);
+                printf("len nome = %d", len);
+                for (int i = 0; i < len; i++) {
+                    nome[i] = atual[i];
+                }
+            }
+
+            if (cont == 2) {
+                len = strlen(atual);
+                for (int i = 0; i < len; i++) {
+                    tel[i] = atual[i];
+                }
+            }
+
+
+            atual = strtok(NULL, " ");
+            cont++;
+        }
+    
+        // FUNÇÕES   
         switch (opt)
         {
         case 'I':
