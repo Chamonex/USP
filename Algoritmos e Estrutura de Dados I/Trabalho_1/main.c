7e9af085b50fdc0c2741ad04bc30463d6fbfec91
Pedro Chamone <chamonepe@gmail.com>
2023-11-06 16:38:55 -0300

    AED: skiplist: alteração na main

--- Algoritmos e Estrutura de Dados I/Trabalho_1/main.c
+++ Algoritmos e Estrutura de Dados I/Trabalho_1/main.c
@@ -52,56 +52,28 @@ void main() {
         switch (opt)
         {
         case 'I':
-            
-            fgets(nome, sizeof(nome), stdin);
-            len = strlen(nome);
-
-            if (nome[len - 1] == '\n')
-                nome[len - 1] = '\0';
-
-            scanf("%d", &tel);
-            getchar();
             insereSkipList(l, nome, tel);
 
             break;
         
         case 'P':
-            fgets(nome, sizeof(nome), stdin);
-            len = strlen(nome);
-
-            if (nome[len - 1] == '\n')
-                nome[len - 1] = '\0';
-
             buscaSkipList(l, nome);
+            
             break;
 
         case 'R':
-            fgets(nome, sizeof(nome), stdin);
-            len = strlen(nome);
-
-            if (nome[len - 1] == '\n')
-                nome[len - 1] = '\0';
-
             removeSkipList(l, nome);
             
             break;
 
         case 'A':
-            
-            fgets(nome, sizeof(nome), stdin);
-            len = strlen(nome);
-
-            if (nome[len - 1] == '\n')
-                nome[len - 1] = '\0';
-
-            scanf("%d", &tel);
-            getchar();
             alteraSkiplist(l, nome, tel);
             
             break;
         
         case '0':
             liberaSkipList(l);
+            
             break;
 
         default:
