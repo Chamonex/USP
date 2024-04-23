ba688c6ab5cc461b5d2434d9476e8239f801e27e
Chamone <chamonepe@gmail.com>
2024-04-23 17:23:28 -0300

    Lista de adjacência AED 2

--- Algoritmos_e_Estrutura_de_Dados_II/Grafos/2-lista_de_adjacencia/adjlist.h
+++ Algoritmos_e_Estrutura_de_Dados_II/Grafos/2-lista_de_adjacencia/adjlist.h
@@ -6,36 +6,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Vertex int  // um Vertex é um int
-
-typedef struct Node *Link;  // o tipo link é um ponteiro 
-                            // para um node!
-// nó da lista de adjacencia
-struct Node {
-    Vertex w;  
-    Link next;  // aponta para  próximo nó
-};
-
-struct Graph {
+#define Vertex int
+#define ERROR 0
+#define SUCCESS 1
+ 
+typedef struct {
     int nVertices;  // quantidade de vértices
     int nArcos;     // quantidade de arcos
-    Link *adj;      // ponteiro para array de vertices
-                    // acaba se tornando uma matriz de
-                    // nodes (matriz de adjacência)
-};
+    Vertex** adj;   // matriz de Vertex
+} Graph;
 
-typedef struct Graph *Digraph;
-#define Graph Digraph   // o grafo também é um dígrafo.
+Graph* initGraph(int);
+Graph* initGraph(int);
+int insertArc(Graph*, Vertex, Vertex);
+int insertEdge(Graph*, Vertex, Vertex);
+int removeArc(Graph*, Vertex, Vertex);
+int removeEdge(Graph*, Vertex, Vertex);
+void printGraph(Graph*);
 
-Graph initGraph(int nV) {
-    // recebe o número de vértices
-    Graph G = (Graph) malloc(sizeof(*G));
-    G->nVertices = nV;
+
+static Vertex** generateAdj(int V) {
+    
+    int i, j;
+    Vertex** m = malloc(V * sizeof(int*));
+
+    for (i = 0; i < V; i++) {
+        m[i] = malloc(V * sizeof(int));
+        for (j = 0; j < V; j++)
+            m[i][j] = 0;
+    }
+
+    return m;
+
+}
+
+
+
+Graph* initGraph(int V) {
+    
+    Graph *G = malloc(sizeof(*G));
+    G->nVertices = V;
     G->nArcos = 0;
-    G->adj = (Link*) malloc(nV * sizeof(Link*)); // matriz de Nodes
+    G-> adj = generateAdj(G->nVertices);
+    return G;
+}
 
-    for (int i = 0; i < nV; i++) {
-        G->adj[i] = NULL;
+// função que cria uma matriz quadratica
+
+int insertArc(Graph *G, Vertex v, Vertex w) {
+    if (G->adj[v][w] == 0) {
+        G->adj[v][w] = 1;
+        G->nArcos++;
+
+        return SUCCESS;
+    }
+    else {
+        printf("The vertex %d - %d already exist\n", v, w);
+        return ERROR;
+    }
+}
+
+int insertEdge(Graph *G, Vertex v, Vertex w){
+    
+    if(insertArc(G, v, w) == 1) {
+        if (insertArc(G, w, v) == 1) {
+            return SUCCESS;
+        }
+    }
+    return ERROR;
+
+}
+
+int removeArc(Graph *G, Vertex v, Vertex w) {
+    if (G->adj[v][w] == SUCCESS) {
+        G->adj[v][w] = 0;
+        G->nArcos--;
+        
+        return SUCCESS;
+    }
+    else {
+        printf("\nThe vertex %d-%d no longer exists\n", v, w);
+        return ERROR;
+    }
+}
+
+int removeEdge(Graph *G, Vertex v, Vertex w) {
+    if (removeArc(G, v, w) == SUCCESS) {
+        if (removeArc(G, w, v) == SUCCESS) {
+            return SUCCESS;
+        }
+    }
+    return ERROR;
+}
+
+void printGraph(Graph *G) {
+    
+    for (int i = 0; i < G->nVertices; i++) {
+        for (int j = 0; j < G->nVertices; j++) {
+            printf("%d ", G->adj[i][j]);
+        }
+        printf("\n");
     }
 }
 
--- Algoritmos_e_Estrutura_de_Dados_II/Grafos/2-lista_de_adjacencia/main.c
+++ Algoritmos_e_Estrutura_de_Dados_II/Grafos/2-lista_de_adjacencia/main.c
@@ -3,8 +3,16 @@
 
 int main() {
 
-
-
-
+    Graph *G = initGraph(6);
+    printGraph(G);
+    insertEdge(G, 1, 2);
+    printf("\n\n\n");
+    printGraph(G);
+    printf("\n\n\n");
+    insertArc(G, 4, 5);
+    printGraph(G);
+    removeEdge(G, 1, 2);
+    removeArc(G, 0, 3);
+    
     return 0;
 }
